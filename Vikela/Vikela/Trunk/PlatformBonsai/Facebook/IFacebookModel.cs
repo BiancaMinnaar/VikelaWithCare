380f54ab8fa7ca42f2b426c8057780d16af42c47
Bianca Minnaar <bminnaar@gmail.com>
2018-07-25 13:44:16 +0200

    Add Facebook Mode and Service - Add IFacebookModel - Add IFacebookService

---
+++ Vikela/Vikela/Trunk/PlatformBonsai/Facebook/IFacebookModel.cs
@@ -0,0 +1,8 @@
+﻿using Vikela.Trunk.Injection.Base;
+
+namespace Vikela.Trunk.PlatformBonsai.Facebook
+{
+    public interface IFacebookModel : IPlatformModelBase
+    {
+    }
+}
---
+++ Vikela/Vikela/Trunk/PlatformBonsai/Facebook/IFacebookService.cs
@@ -0,0 +1,8 @@
+﻿using Vikela.Trunk.Injection.Base;
+
+namespace Vikela.Trunk.PlatformBonsai.Facebook
+{
+    public interface IFacebookService<T> : IPlatformService<T> where T : IPlatformModelBase
+    {
+    }
+}
