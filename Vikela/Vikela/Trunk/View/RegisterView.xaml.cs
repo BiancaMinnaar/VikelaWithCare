95fe848592a24faf41ed1f347fd30b656ebc208d
Bianca Minnaar <bminnaar@gmail.com>
2018-07-26 14:55:46 +0200

    Rename Registration events to correct names.

--- Vikela/Vikela/Trunk/View/RegisterView.xaml.cs
+++ Vikela/Vikela/Trunk/View/RegisterView.xaml.cs
@@ -19,6 +19,21 @@ namespace Vikela.Implementation.View
         {
         }
 
+        public void On_Facebook_Register_Event(object sender, EventArgs e)
+        {
+            _ViewController.Register();
+        }
+
+        public void On_Instagram_Register_Event(object sender, EventArgs e)
+        {
+            _ViewController.Register();
+        }
+
+        public void On_Google_Register_Event(object sender, EventArgs e)
+        {
+            _ViewController.Register();
+        }
+
         public void On_Register_Event(object sender, EventArgs e)
         {
             _ViewController.Register();
