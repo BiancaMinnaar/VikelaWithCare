c5f824a6ddf6c26cf1cfd114d0bf5a4b46b5f10c
Bianca Minnaar <bminnaar@gmail.com>
2018-08-23 08:52:30 +0200

    Add AzureBlobStorageRepository - Add AzureBlobStorageRepository - Add AzureBlobStorageRepository Implementation

---
+++ Vikela/Vikela/Trunk/Repository/IAzureBlobStorageRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Vikela.Trunk.ViewModel;
+
+namespace Vikela.Trunk.Repository
+{
+    public interface IAzureBlobStorageRepository
+    {
+        List<ICloudBlob> GetBlobs();
+        Task GetBlobImageAsync(StoragePictureModel model);
+        Task SaveBlobImageAsync(StoragePictureModel model);
+        void DeleteBlob();
+    }
+}
---
+++ Vikela/Vikela/Trunk/Repository/Implementation/AzureBlobStorageRepository.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using CorePCL;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Vikela.Interface.Repository;
+using Vikela.Root.Repository;
+using Vikela.Trunk.ViewModel;
+
+namespace Vikela.Trunk.Repository.Implementation
+{
+    public class AzureBlobStorageRepository<T> : ProjectBaseRepository, IAzureBlobStorageRepository
+        where T : BaseViewModel
+    {
+        public AzureBlobStorageRepository(IMasterRepository masterRepository)
+            : base(masterRepository)
+        {
+        }
+
+        public void DeleteBlob()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task GetBlobImageAsync(StoragePictureModel model)
+        {
+            var uri = new Uri(model.PictureStorageSASToken.Trim('\"'));
+            CloudBlobContainer container = new CloudBlobContainer(uri);
+            CloudBlockBlob blob = container.GetBlockBlobReference("Picture1.jpg");
+
+            using (var msRead = new MemoryStream())
+            {
+                msRead.Position = 0;
+                using (msRead)
+                {
+                    await blob.DownloadToStreamAsync(msRead);
+                }
+                msRead.Flush();
+                model.UserPicture = msRead.GetBuffer();
+            }
+        }
+
+        public ICloudBlob GetBlobImage(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ICloudBlob> GetBlobs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SaveBlobImageAsync(StoragePictureModel model)
+        {
+            var uri = new Uri(model.PictureStorageSASToken.Trim('\"'));
+            CloudBlobContainer container = new CloudBlobContainer(uri);
+            CloudBlockBlob blob = container.GetBlockBlobReference("Picture1.jpg");
+            await blob.UploadFromByteArrayAsync(model.UserPicture, 0, model.UserPicture.Length);
+        }
+
+        public void SaveBlobImage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
