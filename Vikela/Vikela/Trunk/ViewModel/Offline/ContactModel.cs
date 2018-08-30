b49fc04a47c6fc78f9c58cad9d07029c950273fc
Bianca Minnaar <bminnaar@gmail.com>
2018-08-30 10:45:38 +0200

    Add ContactModel

---
+++ Vikela/Vikela/Trunk/ViewModel/Offline/ContactModel.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace Vikela.Trunk.ViewModel.Offline
+{
+    public class ContactModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+		public string FirstName{ get; set; }
+		public string LastName{ get; set; }
+		public string CellNumber{ get; set; }
+		public string IDNumber{ get; set; }
+    }
+}
