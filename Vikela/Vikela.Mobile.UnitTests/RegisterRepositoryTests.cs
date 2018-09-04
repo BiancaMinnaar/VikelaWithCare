a6ee03cafe419e111f53cf02293031ef36642e5e
Bianca Minnaar <bminnaar@gmail.com>
2018-09-04 11:11:11 +0200

    Add Test for SAS token service call - Add test for RegisterForSASAsync on RegisterRepo

--- Vikela/Vikela.Mobile.UnitTests/RegisterRepositoryTests.cs
+++ Vikela/Vikela.Mobile.UnitTests/RegisterRepositoryTests.cs
@@ -11,6 +11,8 @@ namespace Vikela.Mobile.UnitTests
     [TestClass]
     public class RegisterRepositoryTests
     {
+        const string TokenName = "Bianca Kourtney Minnaar";
+
         MockRepository repository;
         Mock<IMasterRepository> masterRepoMock;
         Mock<IRegisterService> registerService;
@@ -20,19 +22,22 @@ namespace Vikela.Mobile.UnitTests
         {
             repository = new MockRepository(MockBehavior.Strict) { DefaultValue = DefaultValue.Mock };
             masterRepoMock = repository.Create<IMasterRepository>();
-            masterRepoMock.Setup(m => m.GetRegisteredUserOID()).Returns("d0b680d6-95c5-43e3-b036-c850a2696675");
             registerService = repository.Create<IRegisterService>();
             registerRepo = new RegisterRepository(masterRepoMock.Object, registerService.Object);
+        }
+
+        private RegisterViewModel getSimpleRegisterModel()
+        {
+            return new RegisterViewModel()
+            {
+                FirstName = TokenName
+            };
         }
 
         [TestMethod]
         public void TestGetUserFromARTokenReturnsRegisterModel()
         {
-            const string TokenName = "Bianca Kourtney Minnaar";
-            var testModel = new RegisterViewModel()
-            {
-                FirstName = TokenName
-            };
+            masterRepoMock.Setup(m => m.GetRegisteredUserOID()).Returns("d0b680d6-95c5-43e3-b036-c850a2696675");
             var testARToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ilg1ZVhrNHh5b2pORnVtMWtsMll0djhkbE5QNC1jNTdkTzZRR1RWQndhTmsifQ.eyJleHAiOjE1MzU1Mjg2MTgsIm5iZiI6MTUzNTUyNTAxOCwidmVyIjoiMS4wIiwiaXNzIjoiaHR0cHM6Ly9sb2dpbi5taWNyb3NvZnRvbmxpbmUuY29tL2Q5ZGIyMzBiLWU1ZDEtNDU5Ny05MzQ3LTE1MGNmODk4ZWM5YS92Mi4wLyIsInN1YiI6ImQwYjY4MGQ2LTk1YzUtNDNlMy1iMDM2LWM4NTBhMjY5NjY3NCIsImF1ZCI6IjhjMWY3MjIwLTFjYmQtNDVhZC1iNWU1LWIxY2ZkZmNiYjMzZCIsImlhdCI6MTUzNTUyNTAxOCwiYXV0aF90aW1lIjoxNTM1NTI1MDE4LCJuYW1lIjoiQmlhbmNhIEtvdXJ0bmV5IE1pbm5hYXIiLCJpZHAiOiJmYWNlYm9vay5jb20iLCJvaWQiOiJkMGI2ODBkNi05NWM1LTQzZTMtYjAzNi1jODUwYTI2OTY2NzQiLCJ0ZnAiOiJCMkNfMV92aWtlbGFfc2lnbl91cF9pbiJ9.ASO7xBNIqRGRUns_MA9fayVGK4LvWM23Hs3Z-bSuRohVJQLo6hBiqX9dh7fe1fOy1EMNnJ5S1gwHsk4IG1ctkBry4F4yOjlfgq7H4AorngF7-LVTZYVoq4F59GnZbpoqdS43wJjuBfVSdU196pa8NFaEr_kne-KROCkZCPHCj2VRK8K8thwrKMzRM74Ggt9hkcTh0PiJFoMu-dp47Vk9f2FPYbh_5aVvOUgqj69AzmqERKkl6gOeMR1Y5xaLF2c-T6udnNzjxP6ZdH8XkOh4rmnr0NIwcVkUq-HCVBmrOolccWnfzi9VvrlIjxjSnc3mXzaNlfsGPrUyW3HwGtML0Q";
             var testI = new AzureAuthenticationResult()
             {
@@ -45,6 +50,11 @@ namespace Vikela.Mobile.UnitTests
         [TestMethod]
         public void TestCallForImageBlobStorageSASAsyncSetsSASToken()
         {
+            registerService.Setup(s => s.RegisterForSASAsync(It.IsAny<RegisterViewModel>()));
+
+            registerRepo.CallForImageBlobStorageSASAsync(getSimpleRegisterModel());
+
+            repository.VerifyAll();
 
         }
     }
