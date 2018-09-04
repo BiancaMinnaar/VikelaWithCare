20a3099934bc919638b50af0dbec282fcd4a5c63
Bianca Minnaar <bminnaar@gmail.com>
2018-09-04 11:30:41 +0200

    Add Test for SetUserRecordWithRegisterViewModelAsync - add TestSetUserRecordWithRegisterViewModelAsyncSetsUserDataSource

--- Vikela/Vikela.Mobile.UnitTests/RegisterRepositoryTests.cs
+++ Vikela/Vikela.Mobile.UnitTests/RegisterRepositoryTests.cs
@@ -5,6 +5,7 @@ using Vikela.Implementation.ViewModel;
 using Vikela.Interface.Repository;
 using Vikela.Interface.Service;
 using Vikela.Trunk.ViewModel;
+using Vikela.Trunk.ViewModel.Offline;
 
 namespace Vikela.Mobile.UnitTests
 {
@@ -12,7 +13,9 @@ namespace Vikela.Mobile.UnitTests
     public class RegisterRepositoryTests
     {
         const string TokenName = "Bianca Kourtney Minnaar";
-
+        const string OID = "d0b680d6-95c5-43e3-b036-c850a2696675";
+        const string ARToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ilg1ZVhrNHh5b2pORnVtMWtsMll0djhkbE5QNC1jNTdkTzZRR1RWQndhTmsifQ.eyJleHAiOjE1MzU1Mjg2MTgsIm5iZiI6MTUzNTUyNTAxOCwidmVyIjoiMS4wIiwiaXNzIjoiaHR0cHM6Ly9sb2dpbi5taWNyb3NvZnRvbmxpbmUuY29tL2Q5ZGIyMzBiLWU1ZDEtNDU5Ny05MzQ3LTE1MGNmODk4ZWM5YS92Mi4wLyIsInN1YiI6ImQwYjY4MGQ2LTk1YzUtNDNlMy1iMDM2LWM4NTBhMjY5NjY3NCIsImF1ZCI6IjhjMWY3MjIwLTFjYmQtNDVhZC1iNWU1LWIxY2ZkZmNiYjMzZCIsImlhdCI6MTUzNTUyNTAxOCwiYXV0aF90aW1lIjoxNTM1NTI1MDE4LCJuYW1lIjoiQmlhbmNhIEtvdXJ0bmV5IE1pbm5hYXIiLCJpZHAiOiJmYWNlYm9vay5jb20iLCJvaWQiOiJkMGI2ODBkNi05NWM1LTQzZTMtYjAzNi1jODUwYTI2OTY2NzQiLCJ0ZnAiOiJCMkNfMV92aWtlbGFfc2lnbl91cF9pbiJ9.ASO7xBNIqRGRUns_MA9fayVGK4LvWM23Hs3Z-bSuRohVJQLo6hBiqX9dh7fe1fOy1EMNnJ5S1gwHsk4IG1ctkBry4F4yOjlfgq7H4AorngF7-LVTZYVoq4F59GnZbpoqdS43wJjuBfVSdU196pa8NFaEr_kne-KROCkZCPHCj2VRK8K8thwrKMzRM74Ggt9hkcTh0PiJFoMu-dp47Vk9f2FPYbh_5aVvOUgqj69AzmqERKkl6gOeMR1Y5xaLF2c-T6udnNzjxP6ZdH8XkOh4rmnr0NIwcVkUq-HCVBmrOolccWnfzi9VvrlIjxjSnc3mXzaNlfsGPrUyW3HwGtML0Q";
+        private static readonly byte[] pictureBytes = { 1, 2, 33, 44 };
         MockRepository repository;
         Mock<IMasterRepository> masterRepoMock;
         Mock<IRegisterService> registerService;
@@ -37,8 +40,8 @@ namespace Vikela.Mobile.UnitTests
         [TestMethod]
         public void TestGetUserFromARTokenReturnsRegisterModel()
         {
-            masterRepoMock.Setup(m => m.GetRegisteredUserOID()).Returns("d0b680d6-95c5-43e3-b036-c850a2696675");
-            var testARToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ilg1ZVhrNHh5b2pORnVtMWtsMll0djhkbE5QNC1jNTdkTzZRR1RWQndhTmsifQ.eyJleHAiOjE1MzU1Mjg2MTgsIm5iZiI6MTUzNTUyNTAxOCwidmVyIjoiMS4wIiwiaXNzIjoiaHR0cHM6Ly9sb2dpbi5taWNyb3NvZnRvbmxpbmUuY29tL2Q5ZGIyMzBiLWU1ZDEtNDU5Ny05MzQ3LTE1MGNmODk4ZWM5YS92Mi4wLyIsInN1YiI6ImQwYjY4MGQ2LTk1YzUtNDNlMy1iMDM2LWM4NTBhMjY5NjY3NCIsImF1ZCI6IjhjMWY3MjIwLTFjYmQtNDVhZC1iNWU1LWIxY2ZkZmNiYjMzZCIsImlhdCI6MTUzNTUyNTAxOCwiYXV0aF90aW1lIjoxNTM1NTI1MDE4LCJuYW1lIjoiQmlhbmNhIEtvdXJ0bmV5IE1pbm5hYXIiLCJpZHAiOiJmYWNlYm9vay5jb20iLCJvaWQiOiJkMGI2ODBkNi05NWM1LTQzZTMtYjAzNi1jODUwYTI2OTY2NzQiLCJ0ZnAiOiJCMkNfMV92aWtlbGFfc2lnbl91cF9pbiJ9.ASO7xBNIqRGRUns_MA9fayVGK4LvWM23Hs3Z-bSuRohVJQLo6hBiqX9dh7fe1fOy1EMNnJ5S1gwHsk4IG1ctkBry4F4yOjlfgq7H4AorngF7-LVTZYVoq4F59GnZbpoqdS43wJjuBfVSdU196pa8NFaEr_kne-KROCkZCPHCj2VRK8K8thwrKMzRM74Ggt9hkcTh0PiJFoMu-dp47Vk9f2FPYbh_5aVvOUgqj69AzmqERKkl6gOeMR1Y5xaLF2c-T6udnNzjxP6ZdH8XkOh4rmnr0NIwcVkUq-HCVBmrOolccWnfzi9VvrlIjxjSnc3mXzaNlfsGPrUyW3HwGtML0Q";
+            masterRepoMock.Setup(m => m.GetRegisteredUserOID()).Returns(OID);
+            var testARToken = ARToken;
             var testI = new AzureAuthenticationResult()
             {
                 IdToken = testARToken
@@ -56,6 +59,36 @@ namespace Vikela.Mobile.UnitTests
 
             repository.VerifyAll();
 
+        }
+
+        [TestMethod]
+        public void TestSetUserRecordWithRegisterViewModelAsyncSetsUserDataSource()
+        {
+            var regModel = new RegisterViewModel()
+            {
+                FirstName = TokenName,
+                LastName = "Minnaar",
+                MobileNumber = "0835666566",
+                OID = OID,
+                UserPicture = pictureBytes,
+                TokenID = ARToken,
+                PictureStorageSASToken = "Picture SAS"
+            };
+            var actionModel = new UserModel()
+            {
+                FirstName = TokenName,
+                LastName = "Minnaar",
+                MobileNumber = "0835666566",
+                OID = OID,
+                UserPicture = pictureBytes,
+                TokenID = ARToken,
+                PictureStorageSASToken = "Picture SAS"
+            };
+            masterRepoMock.Setup(m => m.SetUserRecordAsync(It.Is<UserModel>(a => a.FirstName == actionModel.FirstName)));
+
+            registerRepo.SetUserRecordWithRegisterViewModelAsync(regModel);
+
+            repository.VerifyAll();
         }
     }
 }
