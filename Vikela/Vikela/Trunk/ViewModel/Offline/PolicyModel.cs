ccfdf0e8cba4fce06ad6a8777aab5a31682121d8
Bianca Minnaar <bminnaar@gmail.com>
2018-09-11 13:45:36 +0200

    Add PolicyModel to offlineModels

---
+++ Vikela/Vikela/Trunk/ViewModel/Offline/PolicyModel.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+
+namespace Vikela.Trunk.ViewModel.Offline
+{
+    public class PolicyModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string name {get;set;}
+        public string startDate {get;set;}
+        public string endDate {get;set;}
+        public string ensuredAmount {get;set;}
+        public string ownerContactId {get;set;}
+        public string ownerProfileImageUrl {get;set;}
+        public string beneficiaryContactId{get;set;}
+        public string beneficiaryProfileImageUrl{get;set;}
+    }
+}
