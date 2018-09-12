a6afbd0235ca43d3d15e52d79129d8247b353806
Bianca Minnaar <bminnaar@gmail.com>
2018-09-12 11:51:37 +0200

    Add ContactStorageRepository

--- Vikela/Vikela/Trunk/Repository/IContactStorageRepository.cs
+++ Vikela/Vikela/Trunk/Repository/IContactStorageRepository.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using System.Threading.Tasks;
+using Vikela.Trunk.ViewModel.Offline;
+
 namespace Vikela.Trunk.Repository
 {
     public interface IContactStorageRepository
     {
+		Task SaveContactToLocalStorage(ContactModel model);
+		Task GetContactWithID(ContactModel model);
     }
 }
--- Vikela/Vikela/Trunk/Repository/Implementation/Offline/ContactStorageRepository.cs
+++ Vikela/Vikela/Trunk/Repository/Implementation/Offline/ContactStorageRepository.cs
@@ -4,11 +4,46 @@ using Vikela.Trunk.ViewModel.Offline;
 
 namespace Vikela.Trunk.Repository.Implementation.Offline
 {
-    public class ContactStorageRepository : BaseStorageRepository<ContactModel>, IUserStorageRepository
+    public class ContactStorageRepository : BaseStorageRepository<ContactModel>, IContactStorageRepository
     {
+        private string ID;
+        protected string SelectWithIDQuery { get; private set; }
+        private const string TrustedSourceRoleName = "Trusted Source";
+        protected string SelectTrustedSources { get; private set; }
+        private const string DefaultBeneficiaryRoleName = "Beneficiary";
+        protected string SelectDefaultBeneficiary { get; private set; }
+
         public ContactStorageRepository(IMasterRepository masterRepository, IOfflineStorageRepository offlineStorageRepo)
             : base(masterRepository, offlineStorageRepo)
         {
+            SelectWithIDQuery = $"SELECT * FROM ContactModel where Id = {ID}";
+            SelectTrustedSources = $"SELECT * FROM ContactModel where ContactRole = '{TrustedSourceRoleName}'";
+            SelectTrustedSources = $"SELECT * FROM ContactModel where ContactRole = '{DefaultBeneficiaryRoleName}'";
+        }
+
+        public async Task GetContactWithID(ContactModel model)
+        {
+            ID = model.Id.ToString();
+            var list = await OfflineStorageRepo.QueryTable<ContactModel>(
+                SelectWithIDQuery);
+            if (list != null && list.Count > 0)
+                model = list[0];
+            model = null;
+        }
+
+        public async Task SaveContactToLocalStorage(ContactModel model)
+        {
+            await CheckCreateModelTable();
+            await GetContactWithID(model);
+            if (model != null)
+            {
+                var affected = await OfflineStorageRepo.UpdateRecord(model);
+                var whatIsAff = affected;
+            }
+            else
+            {
+                await OfflineStorageRepo.InsertRecord(model);
+            }
         }
     }
 }
