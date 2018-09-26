5fd1aa614f5ef10b414d59b29033d95344aa89ec
Bianca Minnaar <bminnaar@gmail.com>
2018-09-26 12:23:01 +0200

    Add more display to PurchaseHistoryTile

--- Vikela/Vikela/Trunk/ViewModel/Controlls/PurchaseHistoryDetailViewModel.cs
+++ Vikela/Vikela/Trunk/ViewModel/Controlls/PurchaseHistoryDetailViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Input;
+﻿using System.Drawing;
+using System.Globalization;
+using System.Windows.Input;
 using Vikela.Implementation.ViewModel;
 using Vikela.Root.ViewModel;
 
@@ -9,5 +11,7 @@ namespace Vikela.Trunk.ViewModel.Controlls
         public int Index { get; set; }
         public PurchaseDetailsViewModel HistoryDetails { get; set; }
         public ICommand ItemClickedCommand { get; set; }
+        public Color TileColor { get; set; }
+        public string CoverMoneyDisplay { get => HistoryDetails.Cover.ToString("C", new CultureInfo("en-ZA")); }
     }
 }
