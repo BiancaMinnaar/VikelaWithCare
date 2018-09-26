0f266c7d37bd453a4565bccf9c1a9bdef8de03f7
Bianca Minnaar <bminnaar@gmail.com>
2018-09-26 13:15:49 +0200

    Fix Tile Values

--- Vikela/Vikela/Trunk/ViewModel/Controlls/PurchaseHistoryDetailViewModel.cs
+++ Vikela/Vikela/Trunk/ViewModel/Controlls/PurchaseHistoryDetailViewModel.cs
@@ -1,8 +1,9 @@
-﻿using System.Drawing;
+﻿using System;
 using System.Globalization;
 using System.Windows.Input;
 using Vikela.Implementation.ViewModel;
 using Vikela.Root.ViewModel;
+using Xamarin.Forms;
 
 namespace Vikela.Trunk.ViewModel.Controlls
 {
@@ -11,7 +12,27 @@ namespace Vikela.Trunk.ViewModel.Controlls
         public int Index { get; set; }
         public PurchaseDetailsViewModel HistoryDetails { get; set; }
         public ICommand ItemClickedCommand { get; set; }
-        public Color TileColor { get; set; }
+        public Color TileColor { get => GetRobotColor(HistoryDetails.StartDate, HistoryDetails.EndDate); }
         public string CoverMoneyDisplay { get => HistoryDetails.Cover.ToString("C", new CultureInfo("en-ZA")); }
+        public string DaysDisplay {
+            get
+            {
+                var timeCalc = TimeSpan.FromTicks(HistoryDetails.EndDate.AddTicks(-HistoryDetails.StartDate.Ticks).Ticks).Days;
+
+                return $"{timeCalc} days";
+            }
+        }
+        public string StartDate { get => HistoryDetails.StartDate.ToString("yyyy-MM-dd", new CultureInfo("en-ZA")); }
+        public string EndDate { get => HistoryDetails.EndDate.ToString("yyyy-MM-dd", new CultureInfo("en-ZA")); }
+
+        Color GetRobotColor(DateTime startTime, DateTime endTime)
+        {
+            var monthsDifference = Math.Abs((startTime.Month - endTime.Month) + 12 * (startTime.Year - endTime.Year));
+            if (monthsDifference == 0)
+                return Color.Red;
+            if (monthsDifference == 1)
+                return Color.Orange;
+            return Color.FromHex("#BBDE6B");
+        }
     }
 }
