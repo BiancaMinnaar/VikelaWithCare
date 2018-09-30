5df1eb48c4fe1c241232f050a680a4a741320d12
Bianca Minnaar <bminnaar@gmail.com>
2018-09-30 09:21:43 +0200

    Implements Communities View. Implement Friends View

--- Vikela/Vikela/Trunk/View/Communitiy/CommunitiesTabView.xaml.cs
+++ Vikela/Vikela/Trunk/View/Communitiy/CommunitiesTabView.xaml.cs
@@ -1,23 +1,42 @@
 using System;
+using System.Collections.Generic;
 using Vikela.Implementation.ViewController;
 using Vikela.Implementation.ViewModel;
 using Vikela.Root.View;
+using Vikela.Trunk.ViewModel.Controlls;
 using Xamarin.Forms;
 
 namespace Vikela.Implementation.View
 {
     public partial class CommunitiesTabView : ProjectBaseContentView<CommunitiesTabViewController, CommunitiesTabViewModel>
     {
+        private int tabIndex = 0;
         public CommunitiesTabView()
         {
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
             BindingContext = _ViewController.InputObject;
+            LoadCareTable();
+        }
+
+        private void LoadCareTable()
+        {
+            var models = new List<ITableScrollItemModel>
+            {
+                new CommunityTileViewModel
+                {
+                    TotalCover = "R30 000",
+                    Name = "Hoenderplaas"
+                }
+            };
+            CareTable.SetTableWithItems(models);
         }
 
         protected override void SetSVGCollection()
         {
         }
+
+        
 
         public async void On_YourMethodName_EventAsync(object sender, EventArgs e)
         {
--- Vikela/Vikela/Trunk/ViewModel/CommunityTileViewModel.cs
+++ Vikela/Vikela/Trunk/ViewModel/CommunityTileViewModel.cs
@@ -1,10 +1,15 @@
 using System;
+using System.Windows.Input;
 using Vikela.Root.ViewModel;
+using Vikela.Trunk.ViewModel.Controlls;
 
 namespace Vikela.Implementation.ViewModel
 {
-    public class CommunityTileViewModel : ProjectBaseViewModel
+    public class CommunityTileViewModel : ProjectBaseViewModel, ITableScrollItemModel
     {
-        
+        public int Index { get; set; }
+        public ICommand ItemClickedCommand { get; set; }
+        public string TotalCover { get; set; }
+        public string Name { get; set; }
     }
 }
